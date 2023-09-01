5c6508ea05ddffba05f07118670fb9a1d5299db0
mehedihasanepu <mehedihasanepu123@gmail.com>
2023-09-01 15:34:27 +0600

    sort by view complete

--- js/ph-tube.js
+++ js/ph-tube.js
@@ -17,11 +17,11 @@ const loadCategoryData = async () => {
 }
 
 
-
 // card section 
 const cardsData = async (id) => {
     const res = await fetch(`https://openapi.programming-hero.com/api/videos/category/${id}`)
     const data = await res.json();
+    datas = data;
     const cardContainer = document.getElementById('card-container');
     cardContainer.innerHTML = '';
     const errorMessage = document.getElementById('no-data-error-message');
@@ -34,7 +34,7 @@ const cardsData = async (id) => {
 
     data.data.forEach((cardData) => {
         const div = document.createElement('div');
-        const verifyImg  = `<img class="w-4 h-4" src="image/verify.png" alt=""></img>`
+        const verifyImg = `<img class="w-4 h-4" src="image/verify.png" alt=""></img>`
         div.innerHTML = `
             <div class="card card-compact bg-base-100 shadow-xl">
             <figure><img class="h-52 w-full" src="${cardData.thumbnail}" alt="Shoes" /></figure>
@@ -48,7 +48,7 @@ const cardsData = async (id) => {
                         <span>${cardData.authors[0].profile_name}</span>
                         <p> ${cardData.authors[0].verified === true ? verifyImg : ''} </p>
                     </div>
-                    <p>${cardData.others.views} views</p>
+                    <p id="views">${cardData.others.views} views</p>
                 </div>
             </div>
         </div>
@@ -57,6 +57,30 @@ const cardsData = async (id) => {
     }
 
     )
+}
+const sortByView = () => {
+    // const views = document.getElementById('views');
+
+    // const viewsNumber = views.innerText.split('K', 1);
+    const cardContainer = document.getElementById('card-container');
+    const cardData = Array.from(cardContainer.children);
+    const cardDataSort = cardData.sort((card1,card2)=>{
+        const viewCount1 = card1.querySelector('#views').innerText.split('K',1);
+        const viewCount2 = card2.querySelector('#views').innerText.split('K',1);
+
+        const viewCount1Int =parseInt(viewCount1);
+        const viewCount2Int =parseInt(viewCount2);
+        console.log(viewCount1Int);
+        console.log(viewCount2Int);
+
+        return viewCount2-viewCount1
+    })
+    cardContainer.innerHTML='';
+
+    cardDataSort.forEach(card=>{
+        cardContainer.appendChild(card);
+    })
+    // cardDataSort()
 }
 
 
