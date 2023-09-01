9c8d61c16005d2f663ff89f1fe9f976724a4d513
mehedihasanepu <mehedihasanepu123@gmail.com>
2023-09-01 16:22:04 +0600

    timer section added

--- js/ph-tube.js
+++ js/ph-tube.js
@@ -7,7 +7,6 @@ const loadCategoryData = async () => {
     categoryData.forEach((category) => {
         const categoryContainer = document.getElementById('category-container');
         const categoryName = category.category;
-        // categoryContainer.classList.add('');
         const div = document.createElement('div');
         div.innerHTML = `
         <button onclick="cardsData(${category.category_id})" class="tab btn btn-sm text-xs px-5 pt-3 pb-7">${categoryName}</button>
@@ -33,11 +32,33 @@ const cardsData = async (id) => {
     }
 
     data.data.forEach((cardData) => {
+        const convertSec = (sec) => {
+            const hrs = Math.floor(sec / 3600);
+            const min = Math.floor((sec % 3600) / 60);
+            return { hrs, min };
+        }
+        const timerData = cardData.others.posted_date;
+        const { hrs, min } = convertSec(timerData);
+        console.log(hrs, min);
+
+
+
+
+
+
+
+
+
+
+
         const div = document.createElement('div');
         const verifyImg = `<img class="w-4 h-4" src="image/verify.png" alt=""></img>`
         div.innerHTML = `
-            <div class="card card-compact bg-base-100 shadow-xl">
+            <div class="card card-compact bg-base-100 shadow-xl relative">
             <figure><img class="h-52 w-full" src="${cardData.thumbnail}" alt="Shoes" /></figure>
+            <div>
+                <p id="timer" class='absolute bottom-32 right-2 px-2 py-1 rounded-sm bg-black text-white text-sm '>${hrs === 0 ? '' : hrs}${hrs === 0 ? '' : ' hrs'}${min === 0 ? '' : min}${min === 0 ? '' : ' min'}${hrs === 0 ? '' : ' ago'}</p>
+            </div>
             <div class="card-body flex flex-row gap-3">
                 <div>
                     <img class="w-10 h-10 rounded-full" src="${cardData.authors[0].profile_picture}" alt="">
@@ -58,30 +79,31 @@ const cardsData = async (id) => {
 
     )
 }
-const sortByView = () => {
-    // const views = document.getElementById('views');
 
-    // const viewsNumber = views.innerText.split('K', 1);
+
+// sort by view section 
+const sortByView = () => {
     const cardContainer = document.getElementById('card-container');
     const cardData = Array.from(cardContainer.children);
-    const cardDataSort = cardData.sort((card1,card2)=>{
-        const viewCount1 = card1.querySelector('#views').innerText.split('K',1);
-        const viewCount2 = card2.querySelector('#views').innerText.split('K',1);
+    const cardDataSort = cardData.sort((card1, card2) => {
+        const viewCount1 = card1.querySelector('#views').innerText.split('K', 1);
+        const viewCount2 = card2.querySelector('#views').innerText.split('K', 1);
 
-        const viewCount1Int =parseInt(viewCount1);
-        const viewCount2Int =parseInt(viewCount2);
+        const viewCount1Int = parseInt(viewCount1);
+        const viewCount2Int = parseInt(viewCount2);
         console.log(viewCount1Int);
         console.log(viewCount2Int);
 
-        return viewCount2-viewCount1
+        return viewCount2 - viewCount1;
     })
-    cardContainer.innerHTML='';
+    cardContainer.innerHTML = '';
 
-    cardDataSort.forEach(card=>{
+    cardDataSort.forEach(card => {
         cardContainer.appendChild(card);
     })
-    // cardDataSort()
 }
+
+
 
 
 loadCategoryData()
